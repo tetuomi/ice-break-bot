8960472a62380b5a91d9822c096362e2e509db3d
tetuomi <kondo.tetta851@mail.kyutech.jp>
2020-02-26 00:18:34 +0900

    score funny face

--- main/line.py
+++ main/line.py
@@ -11,7 +11,7 @@ import random
 
 from main.models import Imagemodel, Modelstatus
 from main.similar import (
-    save_message_id, take_first_message_id, take_first_exist_model, save_exist_model
+    save_message_id, take_first_message_id, take_first_exist_model, save_exist_model, score_funny_face
 )
 
 from pathlib import Path
@@ -37,31 +37,45 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
+
+    if message == "やめる":
+        save_exist_model(False)
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=message))
-
+    
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    message_id = event.message.id
+    print(message_id)
+    message_content = line_bot_api.get_message_content(message_id)
+    message_img_path = f"main/static/{message_id}.jpg"
+    with open(Path(message_img_path).absolute(), "wb") as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+    
     if take_first_exist_model():
+
+        model_id = take_first_message_id()
+        model_img_path = f"main/static/{model_id}.jpg"
+
+        print(model_id)
+        
+        score = score_funny_face(model_img_path, message_img_path)
+        
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=take_first_message_id())
+            TextSendMessage(text='「' + profile.display_name + f'」さんは\n\n{score}点(100)')
         )
-        save_exist_model(False)
+                
+        Path(message_img_path).absolute().unlink()
         
     else:
-        message_id = event.message.id
         save_exist_model(True)
         save_message_id(message_id)
-        
-        message_content = line_bot_api.get_message_content(message_id)
-        file_path = f"main/static/{message_id}.jpg"
-        with open(Path(file_path).absolute(), "wb") as f:
-            for chunk in message_content.iter_content():
-                f.write(chunk)
             
-        profile = line_bot_api.get_profile(event.source.user_id)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='「' + profile.display_name + '」さんの画像を\nモデルにしました')
--- main/similar.py
+++ main/similar.py
@@ -1,4 +1,5 @@
-#import cv2
+import cv2
+import os
 from main.models import Imagemodel, Modelstatus
 from main import db
 
@@ -20,3 +21,24 @@ def take_first_exist_model():
     ms = db.session.query(Modelstatus).all()
     return ms[-1].exist_model
 
+def score_funny_face(model_img_path, compare_img_path):
+    IMG_SIZE = (200, 200)
+    
+    model_img = cv2.imread(model_img_path, cv2.IMREAD_GRAYSCALE)
+    model_img = cv2.resize(model_img, IMG_SIZE)
+
+    compare_img = cv2.imread(compare_img_path, cv2.IMREAD_GRAYSCALE)
+    compare_img = cv2.resize(compare_img, IMG_SIZE)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+    detector = cv2.AKAZE_create()
+    (model_kp, model_des) = detector.detectAndCompute(model_img, None)
+    (compare_kp, compare_des) = detector.detectAndCompute(compare_img, None)
+
+    matches = bf.match(model_des, compare_des)
+    dist = [m.distance for m in matches]
+
+    score = sum(dist) / len(dist)
+
+    return score #点数調整必須
